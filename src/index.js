340617fd1f7db1cba42ad433b7e7a70215417669
AlphaPranav9102 <49816864+AlphaPranav9102@users.noreply.github.com>
2022-10-29 01:52:32 -0400

    removed excess graphs

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ import { Modal } from './components/Modal';
   var modal = new Modal("editModal", "fakeToggle", "getEditedData", "editableFormContainer")
 
   var driverRating = new BarGraph(
-    "plt1",
+    "graphContainer",
     "Avr Driver Rating by Team",
     {
       bar: {
@@ -69,110 +69,4 @@ import { Modal } from './components/Modal';
     },
     modal
   )
-
-  var driverRating = new BarGraph(
-    "plt5",
-    "Avr Driver Rating by Team",
-    {
-      bar: {
-        horizontal: false
-      }
-    },
-    {
-      formula: function(team) {return stats.getAvrStat(team, Queries.DRIVER_RATING)},
-      selectedOptions: [4099, 118, 180],
-      allOptions: Selections.TEAMS
-    },
-    modal
-  )
-
-  var shooterOverTime = new LineGraph(
-    "plt6",
-    "Shooter over matches",
-    {},
-    {
-      formula: function(team) {return stats.getScoreData(team, Queries.TELEOP_UPPER_HUB)},
-      selectedOption: 2056,
-      allOptions: Selections.TEAMS
-    },
-    modal
-  )
-
-  var goodShooters = new ScatterGraph(
-    "plt7",
-    "Shooting by match by team",
-    {},
-    {
-      formulaX: function(team) {return stats.getScoreData(team, Queries.TELEOP_UPPER_HUB)},
-      formulaY: function(team) {return stats.getScoreData(team, Queries.AUTO_UPPER_HUB)},
-      selectedOptions: [4099, 2056],
-      allOptions: Selections.TEAMS
-    },
-    modal
-  )
-
-  var LContribution = new PieGraph(
-    "plt8",
-    "shooting contrib by match",
-    {},
-    {
-      formula: function(match) {return stats.getMatchAllianceData(match, Queries.TELEOP_UPPER_HUB, Selections.RED)},
-      selectedOption: "qm1",
-      allOptions: Selections.MATCHES
-    },
-    modal
-  )
-
-  var plt9 = new BarGraph(
-    "plt9",
-    "Avr Driver Rating by Team",
-    {
-      bar: {
-        horizontal: false
-      }
-    },
-    {
-      formula: function(team) {return stats.getAvrStat(team, Queries.DRIVER_RATING)},
-      selectedOptions: [4099, 118, 180],
-      allOptions: Selections.TEAMS
-    },
-    modal
-  )
-
-  var plt10 = new LineGraph(
-    "plt10",
-    "Shooter over matches",
-    {},
-    {
-      formula: function(team) {return stats.getScoreData(team, Queries.TELEOP_UPPER_HUB)},
-      selectedOption: 2056,
-      allOptions: Selections.TEAMS
-    },
-    modal
-  )
-
-  var plt11 = new ScatterGraph(
-    "plt11",
-    "Shooting by match by team",
-    {},
-    {
-      formulaX: function(team) {return stats.getScoreData(team, Queries.TELEOP_UPPER_HUB)},
-      formulaY: function(team) {return stats.getScoreData(team, Queries.AUTO_UPPER_HUB)},
-      selectedOptions: [4099, 2056],
-      allOptions: Selections.TEAMS
-    },
-    modal
-  )
-
-  var plt12 = new PieGraph(
-    "plt12",
-    "shooting contrib by match",
-    {},
-    {
-      formula: function(match) {return stats.getMatchAllianceData(match, Queries.TELEOP_UPPER_HUB, Selections.RED)},
-      selectedOption: "qm1",
-      allOptions: Selections.MATCHES
-    },
-    modal
-  )
-  })();
+})();
