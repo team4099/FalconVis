63a359a91afcd562e4f623814b557c0c911514cd
AlphaPranav9102 <49816864+AlphaPranav9102@users.noreply.github.com>
2022-11-12 17:37:03 -0500

    added demo of classes to match page

--- src/match/match.js
+++ src/match/match.js
@@ -3,6 +3,9 @@ import { Queries, Selections, JSONData } from '../lib/data/Constants.js'
 import { BarGraph } from '../lib/components/BarGraph.js';
 import { LineGraph } from '../lib/components/LineGraph.js';
 import { Modal } from '../lib/components/Modal.js';
+import { AutomatedMacro } from '../lib/components/AutomatedMacro.js';
+import { WeightedStat } from '../lib/automated/WeightedStat.js'
+import { Factor } from '../lib/automated/Factor.js'
 
 
 (async () => {
@@ -23,8 +26,8 @@ import { Modal } from '../lib/components/Modal.js';
         },
         {
             formulas: {
-              "Teleop Lower": function(team) {return stats.getAvgStat(team, Queries.TELEOP_LOWER_HUB)},
-              "Teleop Upper": function(team) {return stats.getAvgStat(team, Queries.TELEOP_UPPER_HUB)}
+              "Teleop Lower": function(team) {return stats.getAvrStat(team, Queries.TELEOP_LOWER_HUB)},
+              "Teleop Upper": function(team) {return stats.getAvrStat(team, Queries.TELEOP_UPPER_HUB)}
             },
             selectedOptions: blue,
             allOptions: Selections.TEAMS
@@ -56,9 +59,9 @@ import { Modal } from '../lib/components/Modal.js';
         },
         {
             formulas: {
-              "Defense Rating": function(team) {return stats.getAvgStat(team, Queries.DEFENSE_RATING)},
-              "Counter Def Rating": function(team) {return stats.getAvgStat(team, Queries.COUNTER_DEFENSE_RATING)},
-              "Driver Rating": function(team) {return stats.getAvgStat(team, Queries.DRIVER_RATING)}
+              "Defense Rating": function(team) {return stats.getAvrStat(team, Queries.DEFENSE_RATING)},
+              "Counter Def Rating": function(team) {return stats.getAvrStat(team, Queries.COUNTER_DEFENSE_RATING)},
+              "Driver Rating": function(team) {return stats.getAvrStat(team, Queries.DRIVER_RATING)}
             },
             selectedOptions: blue,
             allOptions: Selections.TEAMS
@@ -77,8 +80,8 @@ import { Modal } from '../lib/components/Modal.js';
         },
         {
             formulas: {
-              "Teleop Lower": function(team) {return stats.getAvgStat(team, Queries.TELEOP_LOWER_HUB)},
-              "Teleop Upper": function(team) {return stats.getAvgStat(team, Queries.TELEOP_UPPER_HUB)}
+              "Teleop Lower": function(team) {return stats.getAvrStat(team, Queries.TELEOP_LOWER_HUB)},
+              "Teleop Upper": function(team) {return stats.getAvrStat(team, Queries.TELEOP_UPPER_HUB)}
             },
             selectedOptions: red,
             allOptions: Selections.TEAMS
@@ -110,9 +113,9 @@ import { Modal } from '../lib/components/Modal.js';
         },
         {
             formulas: {
-              "Defense Rating": function(team) {return stats.getAvgStat(team, Queries.DEFENSE_RATING)},
-              "Counter Def Rating": function(team) {return stats.getAvgStat(team, Queries.COUNTER_DEFENSE_RATING)},
-              "Driver Rating": function(team) {return stats.getAvgStat(team, Queries.DRIVER_RATING)}
+              "Defense Rating": function(team) {return stats.getAvrStat(team, Queries.DEFENSE_RATING)},
+              "Counter Def Rating": function(team) {return stats.getAvrStat(team, Queries.COUNTER_DEFENSE_RATING)},
+              "Driver Rating": function(team) {return stats.getAvrStat(team, Queries.DRIVER_RATING)}
             },
             selectedOptions: red,
             allOptions: Selections.TEAMS
@@ -120,6 +123,54 @@ import { Modal } from '../lib/components/Modal.js';
         modal,
         false
     )
+
+    var subDefenseStat = new WeightedStat(
+        [{
+            formula: new Factor(function(team) {return stats.getAvrStat(team, Queries.DEFENSE_RATING)}, 1),
+            weight: 10
+        },
+        {
+            formula: new Factor(function(team) {return stats.getAvrStat(team, Queries.DRIVER_RATING)}, 1),
+            weight: 10
+        },
+        {
+            formula: new Factor(function(team) {return stats.getAvrStat(team, Queries.COUNTER_DEFENSE_RATING)}, 1),
+            weight: 10
+        }],
+        4
+    )
+
+    var defenseStat = new WeightedStat(
+        [{
+            formula: new Factor(function(team) {return stats.getAvrStat(team, Queries.DEFENSE_RATING)}, 1),
+            weight: 10
+        },
+        {
+            formula: new Factor(function(team) {return stats.getAvrStat(team, Queries.DRIVER_RATING)}, 1),
+            weight: 10
+        },
+        {
+            formula: new Factor(function(team) {return stats.getAvrStat(team, Queries.COUNTER_DEFENSE_RATING)}, 1),
+            weight: 10
+        }],
+        4
+    )
+
+    var defenseAutomatedBlue = new AutomatedMacro(
+        "blueAllianceContainer",
+        "Defense Options Auto",
+        defenseStat,
+        blue
+    )
+
+    var defenseAutomatedRed = new AutomatedMacro(
+        "redAllianceContainer",
+        "Defense Options Auto",
+        defenseStat,
+        red
+    )
+
+
 
     var setTeams = () => {
         blue = [
@@ -141,6 +192,9 @@ import { Modal } from '../lib/components/Modal.js';
         teleopCargoRed.pushEdit(false, red)
         teleopPOTRed.pushEdit(false, red)
         defenseRed.pushEdit(false, red)
+
+        defenseAutomatedBlue.pushEdit(blue)
+        defenseAutomatedRed.pushEdit(red)
     }
 
     document.getElementById("setTeams").addEventListener("click", setTeams)
