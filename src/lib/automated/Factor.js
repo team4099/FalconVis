c3a9490f8bfaf2f654bbe540803e28bfd9ee9b67
AlphaPranav9102 <49816864+AlphaPranav9102@users.noreply.github.com>
2022-11-13 11:24:10 -0500

    added factor jsdocs

--- src/lib/automated/Factor.js
+++ src/lib/automated/Factor.js
@@ -1,9 +1,20 @@
+/** Class representing an individual point of a statistical criteria. */
 class Factor{
+    /**
+     * Create a point.
+     * @callback formula - Anonymous function to get statistical data based on team
+     * @param {number} bias - Multiplier on a factor internally to possible modify data to convert to score or other metric
+     */
     constructor(formula, bias){
         this.formula = formula
         this.bias = bias
     }
 
+    /**
+     * Get the dot's width.
+     * @param {number} team - Team number to be passed into formula (callback)
+     * @return {number} Return f(team) value with modification by bias
+     */
     getValue(team){
         return this.formula(team) * this.bias
     }
