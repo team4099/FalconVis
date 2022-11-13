f5f8e4aff5adedeef15524709a79045d0ecbc335
AlphaPranav9102 <49816864+AlphaPranav9102@users.noreply.github.com>
2022-11-13 11:49:45 -0500

    Fixed type issues with composite stat docs

--- src/lib/automated/CompositeStat.js
+++ src/lib/automated/CompositeStat.js
@@ -3,7 +3,7 @@ class CompositeStat{
     /**
      * Create CompositeStat
      * @param {Factor[]} factors - List of Factors in criteria
-     * @param {number} threshold - setting boolean threshold of conditional if team passes criteria 
+     * @param {Number} threshold - setting boolean threshold of conditional if team passes criteria 
      */
     constructor(factors, threshold){
         this.factors = factors
@@ -13,8 +13,8 @@ class CompositeStat{
 
     /**
      * Calls each factor in this.factors sums up and gets criteria results
-     * @param {*} team - Team for what the criteria is being evaluated for
-     * @return {boolean} - If team passes critera based on threshold
+     * @param {Number} team - Team for what the criteria is being evaluated for
+     * @return {Boolean} - If team passes critera based on threshold
      */
     getValue(team){
         var sum = 0
