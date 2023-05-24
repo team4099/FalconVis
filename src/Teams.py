b8ae2b008499fe8777df8887847ebe15d29909b3
Shayaan Wadkar <82843611+Shom770@users.noreply.github.com>
2023-05-24 13:31:57 -0400

    create new structure for swapping between cycle contribution and point contribution graphs

--- src/Teams.py
+++ src/Teams.py
@@ -3,6 +3,7 @@
 import streamlit as st
 
 from page_managers import TeamManager
+from utils import GraphType
 
 # Configuration for Streamlit
 st.set_page_config(
@@ -19,10 +20,12 @@ if __name__ == '__main__':
     # Generate the input section of the `Teams` page.
     team_number = team_manager.generate_input_section()
 
-    metric_tab, graphs_tab = st.tabs(["📊 Metrics", "📈 Graphs"])
+    metric_tab, auto_graphs_tab, teleop_graphs_tab = st.tabs(
+        ["📊 Metrics", "🤖 Autonomous Graphs", "🎮 Teleop + Endgame Graphs"]
+    )
 
     with metric_tab:
-        st.write("## Metrics")
+        st.write("### Metrics")
 
         quartile = st.select_slider(
             "Quartile for Comparisons (The deltas for each metric represent the deviation from said quartile).",
@@ -30,7 +33,23 @@ if __name__ == '__main__':
         ) / 100
         team_manager.generate_metrics(team_number, quartile)
 
-    with graphs_tab:
-        st.write("## Graphs")
+    with auto_graphs_tab:
+        st.write("### 🤖 Autonomous Graphs")
         
-        team_manager.generate_graphs(team_number)
+        # Create cycle contribution and point contribution graph tabs.
+        auto_cycle_contrib_tab, auto_point_contrib_tab = st.tabs(
+            ["📈 Cycle Contribution Graphs", "🧮 Point Contribution Graphs"]
+        )
+        
+        with auto_cycle_contrib_tab:
+            team_manager.generate_autonomous_graphs(
+                team_number,
+                type_of_graph=GraphType.CYCLE_CONTRIBUTIONS
+            )
+        
+        with auto_point_contrib_tab:
+            team_manager.generate_autonomous_graphs(
+                team_number,
+                type_of_graph=GraphType.POINT_CONTRIBUTIONS
+            )
+            
