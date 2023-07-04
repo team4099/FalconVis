ceccef089370b0fc7ae32c00e8453afe6ce17352
Shilab66 <89350258+Shilab66@users.noreply.github.com>
2023-07-04 13:24:14 -0400

    created a regenerable picklist

--- src/pages/4_Picklist.py
+++ src/pages/4_Picklist.py
@@ -2,6 +2,9 @@
 
 import streamlit as st
 from page_managers import PicklistManager
+from utils import retrieve_scouting_data
+import pandas as pd
+from st_aggrid import AgGrid, GridOptionsBuilder
 
 # Configuration for Streamlit
 st.set_page_config(
@@ -11,9 +14,51 @@ st.set_page_config(
 )
 picklist_manager = PicklistManager()
 
+def convert_df(df):
+   return df.to_csv(index=False).encode('utf-8')
+
+
 if __name__ == '__main__':
     # Write the title of the page.
     st.write("# Picklist")
 
     # Generate the input section of the `Picklist` page.
     fields_selected = picklist_manager.generate_input_section()
+
+    # exportButton = st.button("Export to csv")
+
+    teamsList = []
+    for index, row in retrieve_scouting_data().iterrows():
+        if row.TeamNumber not in teamsList:
+            teamsList.append(row.TeamNumber)
+
+    autoGridDf = pd.DataFrame({'Team Number': [], 'Auto Cycles': [], 'Teleop Cycles': []})
+
+    for i in teamsList:
+       new = [i, picklist_manager.calculated_stats.average_cycles(i, "AutoGrid"), picklist_manager.calculated_stats.average_cycles(i, "TeleopGrid")]
+       autoGridDf.loc[len(autoGridDf)] = new
+    autoGridDf = autoGridDf.sort_values(by=['Auto Cycles'], ascending=False)
+
+    if("Average Teleop Cycles" not in fields_selected):
+        autoGridDf = autoGridDf.drop(['Teleop Cycles'], axis=1)
+    if("Average Auto Cycles" not in fields_selected):
+            autoGridDf = autoGridDf.drop(['Auto Cycles'], axis=1)
+
+    grid_return = AgGrid(autoGridDf, key='df')
+    df = grid_return["data"]
+
+    csv = convert_df(df)
+
+    st.download_button(
+       "Press to Download",
+       csv,
+       "file.csv",
+       "text/csv",
+       key='download-csv'
+    )
+
+
+
+
+
+
