ed987d4ed995af287ac948a9758dc8bc22b51db9
Shilab66 <89350258+Shilab66@users.noreply.github.com>
2023-10-14 14:28:50 -0400

    second attempt to fix displaying note scouting error

--- src/utils/functions.py
+++ src/utils/functions.py
@@ -71,7 +71,7 @@ def retrieve_note_scouting_data() -> DataFrame:
     scouting_data = DataFrame.from_dict(
         get(EventSpecificConstants.NOTE_SCOUTING_URL).json()
     )
-    if scouting_data[Queries.MATCH_NUMBER] != None:
+    if scouting_data != None:
         scouting_data[Queries.MATCH_NUMBER] = scouting_data[Queries.MATCH_KEY].apply(
             lambda match_key: int(search(r"\d+", match_key).group(0))
         )
