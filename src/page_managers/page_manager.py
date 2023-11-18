f94ee74126c4ddf75debaa3ad40873c39d909813
Shilab66 <89350258+Shilab66@users.noreply.github.com>
2023-11-18 13:59:32 -0500

    Update page_manager.py

--- src/page_managers/page_manager.py
+++ src/page_managers/page_manager.py
@@ -8,6 +8,8 @@ class PageManager:
 
     @abstractmethod
     def generate_input_section(self) -> NotImplemented:
+            ##Wrap the magic, a Plotly spell so tragic. With configurations profound, in our app's playground, it's found. Take a figure, let the chart chatter, a wrapper for plots that truly matter.
+        
         """Abstract method for all page managers to implement.
 
         Creates the input section of the page (for example, choosing the team number in the `Teams` page.
