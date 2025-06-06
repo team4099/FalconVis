6ab8a1f64919b4dc4aeefd0eb06d6c2f99d4aac5
Ryan Lo <ytryanlo@gmail.com>
2025-06-05 22:06:35 -0400

    Completed scouting accuracy page

--- src/page_managers/scouting_accuracy_manager.py
+++ src/page_managers/scouting_accuracy_manager.py
@@ -1,4 +1,5 @@
 """Creates the `ScoutingAccuracyManager` class used to set up the Scouting Accuracy page and generate its table."""
+import pandas as pd
 import streamlit as st
 from .page_manager import PageManager
 from utils import (
@@ -25,7 +26,6 @@ class ScoutingAccuracyManager(PageManager):
         self.match_schedule = retrieve_match_schedule()
         self.match_data = retrieve_match_data()
 
-
     def generate_input_section(self) -> str:
         """Generates the input section of the `Scouting Accuracy` page.
 
@@ -41,6 +41,13 @@ class ScoutingAccuracyManager(PageManager):
 
     def generate_accuracy_table(self, member_name) -> DataFrame:
         """Generates the scouting accuracy table for the `Scouting Accuracy` page."""
+
+        accuracy_dict = {
+            'ScoutersNames': [],
+            'CumulativeAccuracy': [],
+            'NumberOfScoutedMatches': []
+        }
+
         for index, row in self.match_data.iterrows():
             match_key = row["match_key"]
             red_alliance = row["red_alliance"]
@@ -63,6 +70,8 @@ class ScoutingAccuracyManager(PageManager):
 
             red_scouting_alliance_score = 0
 
+            scouters_names_list_r = []
+
             for team_key in team_list:
                 scouting_team_filter = self.raw_scouting_data[self.raw_scouting_data[Queries.TEAM_NUMBER] == int(team_key)]
                 scouting_team_filter = scouting_team_filter.reset_index(drop=True)
@@ -71,9 +80,21 @@ class ScoutingAccuracyManager(PageManager):
                     match_index = match_index_list[0]
                     points_per_match = self.calculated_stats.points_contributed_by_match(int(team_key)).values
                     red_scouting_alliance_score += points_per_match[match_index]
+                    scout_name = scouting_team_filter.iloc[match_index][Queries.SCOUT_ID]
+                    scouters_names_list_r.append(scout_name)
 
-            red_alliance_accuracy = (red_scouting_alliance_score-red_calculated_score)/red_calculated_score
+            red_alliance_accuracy = (1 - abs(red_scouting_alliance_score-red_calculated_score)/red_calculated_score) * 100
+            scouters_names = ", ".join(scouters_names_list_r)
 
+            if member_name in scouters_names:
+                if scouters_names not in accuracy_dict['ScoutersNames']:
+                    accuracy_dict['ScoutersNames'].append(scouters_names)
+                    accuracy_dict['CumulativeAccuracy'].append(red_alliance_accuracy)
+                    accuracy_dict['NumberOfScoutedMatches'].append(1)
+                else:
+                    accuracy_scouts_index = accuracy_dict['ScoutersNames'].index(scouters_names)
+                    accuracy_dict['CumulativeAccuracy'][accuracy_scouts_index] += red_alliance_accuracy
+                    accuracy_dict['NumberOfScoutedMatches'][accuracy_scouts_index] += 1
 
             # Blue Alliance score from TBA
             team_list = blue_alliance.split(",")
@@ -87,6 +108,8 @@ class ScoutingAccuracyManager(PageManager):
 
             blue_scouting_alliance_score = 0
 
+            scouters_names_list_b = []
+
             for team_key in blue_alliance.split(","):
                 scouting_team_filter = self.raw_scouting_data[self.raw_scouting_data[Queries.TEAM_NUMBER] == int(team_key)]
                 scouting_team_filter = scouting_team_filter.reset_index(drop=True)
@@ -95,7 +118,29 @@ class ScoutingAccuracyManager(PageManager):
                     match_index = match_index_list[0]
                     points_per_match = self.calculated_stats.points_contributed_by_match(int(team_key)).values
                     blue_scouting_alliance_score += points_per_match[match_index]
+                    scout_name = scouting_team_filter.iloc[match_index][Queries.SCOUT_ID]
+                    scouters_names_list_b.append(scout_name)
+
                 self.calculated_stats.points_contributed_by_match(team_key)
                 blue_scouting_alliance_score += self.calculated_stats.points_contributed_by_match(team_key).sum()
 
-            blue_alliance_accuracy = (blue_scouting_alliance_score-blue_calculated_score)/blue_calculated_score
+            blue_alliance_accuracy = (1 - abs(blue_scouting_alliance_score-blue_calculated_score)/blue_calculated_score) * 100
+
+            scouters_names = ", ".join(scouters_names_list_b)
+            if member_name in scouters_names:
+                if scouters_names not in accuracy_dict['ScoutersNames']:
+                    accuracy_dict['ScoutersNames'].append(scouters_names)
+                    accuracy_dict['CumulativeAccuracy'].append(blue_alliance_accuracy)
+                    accuracy_dict['NumberOfScoutedMatches'].append(1)
+                else:
+                    accuracy_scouts_index = accuracy_dict['ScoutersNames'].index(scouters_names)
+                    accuracy_dict['CumulativeAccuracy'][accuracy_scouts_index] += blue_alliance_accuracy
+                    accuracy_dict['NumberOfScoutedMatches'][accuracy_scouts_index] += 1
+
+        df = pd.DataFrame(data={
+            'Scouters': accuracy_dict['ScoutersNames'],
+            'Average Accuracy %': [accuracy_dict['CumulativeAccuracy'][scouter_set]/accuracy_dict['NumberOfScoutedMatches'][scouter_set] for scouter_set in range(len(accuracy_dict['NumberOfScoutedMatches']))],
+            'NumberOfScoutedMatches': accuracy_dict['NumberOfScoutedMatches']
+        })
+
+        return df
