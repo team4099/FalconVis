4bd1bb54195e0b67b1329036514cf9be4eebf275
00magikarp <94652654+00magikarp@users.noreply.github.com>
2025-06-19 23:26:18 -0400

    setup for dod

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -53,8 +53,8 @@ class GeneralConstants:
 
 class EventSpecificConstants:
     """Constants specific to an event."""
-    EVENT_CODE = "2025vapor"
-    EVENT_NAME = "Portsmouth"
+    EVENT_CODE = "2025njdd"
+    EVENT_NAME = "Duel on the Delaware"
     URL = f"https://raw.githubusercontent.com/team4099/ScoutingAppData/main/{EVENT_CODE}_match_data.json"
     NOTE_SCOUTING_URL = f"https://raw.githubusercontent.com/team4099/ScoutingAppData/main/{EVENT_CODE}_qualitative_data.json"
     PIT_SCOUTING_URL = (
