3a74fe2095dcb13bf043197918ea9251bc935e1b
CodingMaster121 <67883747+CodingMaster121@users.noreply.github.com>
2025-08-31 16:48:12 -0400

    Update constants.py

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -7,7 +7,6 @@ __all__ = [
     "EventSpecificConstants",
     "GeneralConstants",
     "GraphType",
-    "NoteScoutingQueries",
     "Queries"
 ]
 
@@ -19,7 +18,7 @@ class GeneralConstants:
         "Average Teleop Cycles",
         "Average Auto Cycles"
     ]
-    SECONDS_TO_CACHE = 60 * 4
+    SECONDS_TO_CACHE = 60 * 1.5
     PRIMARY_COLOR = "#EFAE09"
     AVERAGE_FOUL_RATE = 1.06
 
@@ -27,6 +26,7 @@ class GeneralConstants:
     RED_ALLIANCE_GRADIENT = ["#731111", "#b04949", "#ed8282", "#f5b3b3"]
     BLUE_ALLIANCE_GRADIENT = ["#0b2e61", "#355687", "#7da0d1", "#a8c1e3"]
     GOLD_GRADIENT = ["#ffbd4d", "#ff9000", "#dd5f00"]
+    GREEN_TO_PURPLE_GRADIENT = ["#68D391", "#B794F4"]
     LEVEL_GRADIENT = ["#f44a53", "#ff8800", "#f4c717"]
     RED_TO_GREEN_GRADIENT = ["#ffb6b3", "#ffd5d4", "#e7f1e8", "#bde7bd", "#77dd76"]
     SHORT_RED_TO_GREEN_GRADIENT = ["#ffb6b3", "#ffd5d4", "#bde7bd", "#77dd76"]
@@ -42,128 +42,95 @@ class GeneralConstants:
     CONE_COLOR = PRIMARY_COLOR
     CUBE_COLOR = "#4F46E5"
 
-    # Game-specific constants
-    CHARGE_STATION_LENGTH = 8  # In feet
+    # General game constants
+    TELEOP_TOTAL_TIME = (2 * 60 + 15)
+    TELEOP_MINUS_ENDGAME = TELEOP_TOTAL_TIME - 20
 
-    # Qualitative data constants
-    POSITIVE_TERMS = {"consistent", "speed", "good", "cycle", "fast", "score", "well"}
-    NEGATIVE_TERMS = {"can't", "disable", "foul", "bad", "drop", "stuck", "poor", "missed", "slow", "only", "tip", "broke", "struggle", "bug"}
+    # Sentiment analysis terms
+    POSITIVE_TERMS = {"consistent", "speed", "good", "cycle", "fast", "score", "well", "amazing", "spectactular"}
+    NEGATIVE_TERMS = {"can't", "disable", "foul", "bad", "drop", "stuck", "poor", "missed", "slow", "only", "tip", "broke", "struggle", "bug", "prone"}
 
 
 class EventSpecificConstants:
     """Constants specific to an event."""
-
-    EVENT_CODE = "2022mockdata"
+    EVENT_CODE = "2025vapor"
+    EVENT_NAME = "Portsmouth"
     URL = f"https://raw.githubusercontent.com/team4099/ScoutingAppData/main/{EVENT_CODE}_match_data.json"
+    NOTE_SCOUTING_URL = f"https://raw.githubusercontent.com/team4099/ScoutingAppData/main/{EVENT_CODE}_qualitative_data.json"
+    PIT_SCOUTING_URL = (
+        f"https://raw.githubusercontent.com/team4099/ScoutingAppData/main/{EVENT_CODE}_pit_scouting_data.csv"
+    )
+    PICKLIST_URL = "https://www.notion.so/team4099/d19066533a8844d3aa2cd9e68e70f214?v=56e109b2298d46ebb00057f05d38bba8"
 
 
 class GraphType(Enum):
     """Enum class representing the different graph types (cycle contribution graphs / point contribution graphs)."""
-    
+
     CYCLE_CONTRIBUTIONS = 0
     POINT_CONTRIBUTIONS = 1
-    
+
 
 class Queries:
     """Constants specific to fields in the scouting data."""
 
     # Constants relating to fields
-    MATCH_KEY = "match_key"
-    MATCH_NUMBER = "match_number"
-    TEAM_NUMBER = "team_number"
-    SCOUT_ID = "scout_id"
-    ALLIANCE = "alliance"
-    DRIVER_STATION = "driver_station"
+    SCOUT_ID = "ScoutId"
+    MATCH_KEY = "MatchKey"
+    MATCH_NUMBER = "MatchNumber"
+    TEAM_NUMBER = "TeamNumber"
 
-    # Year specific data points
-    AUTO_UPPER_HUB = "auto_upper_hub"
-    AUTO_LOWER_HUB = "auto_lower_hub"
-    AUTO_MISSES = "auto_misses"
-    TAXIED = "taxied"
+    STARTING_POSITION = "StartingPosition"
+    LEFT_STARTING_ZONE = "AutoLeave"
+    SCORING_SIDE = "ScoringSide"
+    AUTO_CORAL_L1 = "AutoCoralL1"
+    AUTO_CORAL_L2 = "AutoCoralL2"
+    AUTO_CORAL_L3 = "AutoCoralL3"
+    AUTO_CORAL_L4 = "AutoCoralL4"
+    AUTO_CORAL_MISSES = "AutoCoralMisses"
+    AUTO_BARGE = "AutoBarge"
+    AUTO_PROCESSOR = "AutoProcessor"
 
-    TELEOP_LOWER_HUB = "teleop_lower_hub"
-    TELEOP_UPPER_HUB = "teleop_upper_hub"
-    TELEOP_MISSES = "teleop_misses"
+    TELEOP_CORAL_L1 = "TeleopCoralL1"
+    TELEOP_CORAL_L2 = "TeleopCoralL2"
+    TELEOP_CORAL_L3 = "TeleopCoralL3"
+    TELEOP_CORAL_L4 = "TeleopCoralL4"
+    TELEOP_CORAL_MISSES = "TeleopCoralMisses"
+    TELEOP_BARGE = "TeleopAlgaeBarge"
+    TELEOP_PROCESSOR = "TeleopAlgaeProcessor"
+    TELEOP_ALGAE_REMOVAL = "TeleopAlgaeRemoval"
 
-    FINAL_CLIMB_TYPE = "final_climb_type"
-    CLIMB_TIME = "climb_time"
-    DEFENSE_RATING = "defense_rating"
-    DEFENSE_PERCENT = "defense_pct"
-    COUNTER_DEFENSE_RATING = "counter_defense_rating"
-    COUNTER_DEFENSE_PERCENT = "counter_defense_pct"
+    PARKED_UNDER_BARGE = "Parked"
+    CLIMBED_CAGE = "ClimbStatus"
+    CLIMB_SPEED = "ClimbSpeed"
+
+    DRIVER_RATING = "DriverRating"
+    ROBOT_STYLE_TYPE = "RobotStyleType"
+    INTAKE_SPEED = "IntakeSpeed"
+    DEFENSE_RATING = "DefenseRating"
+    INTAKE_DEFENSE_RATING = "IntakeDefenseRating"
+    DISABLE = "Disabled"
+    STABILITY = "StabilityRating"
+
+    # Notes
+    AUTO_NOTES = "AutoNotes"
+    TELEOP_NOTES = "TeleopNotes"
+    ENDGAME_NOTES = "EndgameNotes"
+    RATING_NOTES = "RatingNotes"
 
     # Alliance constants
     RED_ALLIANCE = "red"
     BLUE_ALLIANCE = "blue"
 
-    # Mode
-    AUTO = "auto"
-    TELEOP = "teleop"
+    # Modes
+    AUTO = "Auto"
+    AUTO_CORAL = "AutoCoral"
+    TELEOP = "Teleop"
+    ENDGAME = "Endgame"
 
-    # Heights
-    UPPER_HUB = "upper_hub"
-    LOWER_HUB = "lower_hub"
-
-    # Different climb types   
-    CLIMB_TYPES = ["Low", "Mid", "High", "Traversal"]
-
-
-class NoteScoutingQueries:
-    """Constants specific to fields in the note scouting data."""
-    AUTO_GRID = "AutoPieces"
-    AUTO_ENGAGED = "AutoEngaged"
-    AUTO_INTAKE_ACCURACY = "AutoIntakeAccuracy"
-    AUTO_DRIVING_SKILLS = "AutoDrivingSkills"
-    AUTO_STARTING_POSITION = "AutoStartingPosition"
-    AUTO_SCORING_ACCURACY = "AutoScoringAccuracy"
-
-    TELEOP_GRID = "TeleopPieces"
-    TELEOP_PATH = "TeleopPath"
-    TELEOP_ALIGNING_SPEED = "TeleopAligningSpeed"
-    TELEOP_INTAKING_LOCATION = "TeleopIntakingLocation"
-    COMMUNITY_DRIVING_SKILLS = "TeleopCommunitySkill"
-
-    DISABLED = "Disabled"
-    TIPPY = "Tippy"
-    DRIVER_RATING = "DriverRating"
-    CONE_INTAKING_SKILLS = "ConeIntakingSkill"
-    CUBE_INTAKING_SKILLS = "CubeIntakingSkill"
-    LOADING_ZONE_SPEED = "SubstationSpeed"
-
-    CHOICE_NAMES = {
-        AUTO_STARTING_POSITION: ["Cable protector side", "Charging station", "Loading zone side"],
-        AUTO_SCORING_ACCURACY: ["Lousy", "Poor", "Okay", "Decent", "Great"],
-        DRIVER_RATING: ["Very Poor", "Poor", "Average", "Fluid", "Very Fluid"],
-        TELEOP_PATH: [
-            "No specific path",
-            "Cable protector side to loading zone",
-            "Over the charging station to loading zone",
-            "No cable protector side to loading zone"
-        ],
-        TELEOP_ALIGNING_SPEED: ["Very Slow", "Slow", "Fast", "Quick"],
-        COMMUNITY_DRIVING_SKILLS: ["Very Poor", "Poor", "Well", "Smoothly"],
-        TELEOP_INTAKING_LOCATION: ["Ground", "Single substation", "Double substation"],
-        CONE_INTAKING_SKILLS: ["Very Poor", "Poor", "Average", "Good", "Very Good"],
-        CUBE_INTAKING_SKILLS: ["Very Poor", "Poor", "Average", "Good", "Very Good"],
-        LOADING_ZONE_SPEED: ["Very Poor", "Poor", "Average", "Good", "Very Good"],
-    }
-
-    @staticmethod
-    def classify_driver_rating_from_decimal(rating: float) -> str:
-        """Returns the corresponding name for the driver rating based on the rating passed in (like 4.2 -> Very Fluid).
-
-        :param rating: A float representing the rating that should be converted.
-        """
-        if rating >= 4.5:
-            return "Very Fluid"
-        elif 4 <= rating < 4.5:
-            return "Fluid"
-        elif 3 <= rating < 4:
-            return "Average"
-        elif 2 <= rating < 3:
-            return "Poor"
-        elif 1 <= rating < 2:
-            return "Very Poor"
+    # Custom graph keywords
+    ONE_TEAM_KEYWORD = "Used for custom graphs with one team."
+    THREE_TEAMS_KEYWORD = "Used for custom graphs with three teams."
+    FULL_EVENT_KEYWORD = "Used for custom graphs with a full event."
 
 
 class Criteria:
@@ -174,13 +141,50 @@ class Criteria:
         0: 0,
         "false": 0,
         1: 1,
-        "true": 1
+        "true": 1,
+        False: 0,
+        True: 1
     }
 
     # Endgame Criteria
-    ENDGAME_POINTAGE = {
-        "Low": 4,
-        "Mid": 6,
-        "High": 10,
-        "Traversal": 15
+    CLIMBING_POINTAGE = {
+        "No Climb": 2,
+        "Shallow Climb": 6,
+        "Deep Climb": 12
+    }
+
+    CLIMBING_CRITERIA = {
+        "No Climb": 0,
+        "Shallow Climb": 1,
+        "Deep Climb": 1
+    }
+
+    # Ratings criteria
+    DRIVER_RATING_CRITERIA = {
+        "Very Fluid": 5,
+        "Fluid": 4,
+        "Average": 3,
+        "Poor": 2,
+        "Very Poor": 1
+    }
+    INTAKE_SPEED_CRITERIA = {
+        "Very Fast": 5,
+        "Fast": 4,
+        "Average": 3,
+        "Slow": 2,
+        "Very Slow": 1
+    }
+    DEFENSE_TIME_CRITERIA = {
+        "Very Often": 5,
+        "Often": 4,
+        "Sometimes": 3,
+        "Rarely": 2,
+        "Never": 1
+    }
+    BASIC_RATING_CRITERIA = {
+        "Very Good": 5,
+        "Good": 4,
+        "Okay": 3,
+        "Poor": 2,
+        "Very Poor": 1
     }
