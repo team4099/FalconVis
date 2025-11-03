56b11249939b02301176230925ae89a77f5ed6bb
Ryan Lo <ytryanlo@gmail.com>
2025-11-03 15:33:16 -0500

    added a column for auto accuracy

--- src/page_managers/scouting_accuracy_manager.py
+++ src/page_managers/scouting_accuracy_manager.py
@@ -185,6 +185,7 @@ class ScoutingAccuracyManager(PageManager):
         df = pd.DataFrame(data={
             'Scouters': accuracy_dict['ScoutersNames'],
             'Average Accuracy %': [round(accuracy_dict['CumulativeAccuracy'][scouter_set]/accuracy_dict['NumberOfScoutedMatches'][scouter_set], 2) for scouter_set in range(len(accuracy_dict['NumberOfScoutedMatches']))],
+            'Average Auto Accuracy %': [round(accuracy_dict['AutoAccuracy'][scouter_set]/accuracy_dict['NumberOfScoutedMatches'][scouter_set], 2) for scouter_set in range(len(accuracy_dict['NumberOfScoutedMatches']))],
             'NumberOfScoutedMatches': accuracy_dict['NumberOfScoutedMatches']
         })
 
