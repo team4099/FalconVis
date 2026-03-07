5afe3306ea8d8a1c7e33f0bc50a65c101662515f
CodingMaster121 <ytryanlo@gmail.com>
2026-03-07 17:19:45 -0500

    fixing individual page issues

--- src/page_managers/team_manager.py
+++ src/page_managers/team_manager.py
@@ -67,7 +67,7 @@ class TeamManager(PageManager, ContainsMetrics):
         tba_match_lookup = {}
 
         for match in tba_matches:
-            if match['score_breakdown'] is not None:
+            if type(match['score_breakdown']) is not dict:
                 tba_match_lookup[f"{match['comp_level']}{match['match_number']}"] = match
 
         tba_scaled_points_by_team = {}
